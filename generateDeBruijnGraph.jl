20a50ae2f02e8d48ca8a40371634337e0c202ee9
Mae <merimaat1@gmail.com>
2020-09-01 01:00:37 -0400

    Added initial scripts to be tested

---
+++ generateDeBruijnGraph.jl
@@ -0,0 +1,38 @@
+#!/usr/bin/env julia
+
+#Add packages
+Pkg.add("GenomeGraphs")
+Pkg.add("FASTX")
+Pkg.add("ReadDatastores")
+
+#Load packages
+using GenomeGraphs
+using FASTX
+using ReadDatastores
+
+#Set workspace
+ws=WorkSpace()
+
+#Retrieve inputs
+leftRead=ARGS[1]
+rightRead=ARGS[2]
+outputGraph=ARGS[3]
+
+#Read input left and right trimmed reads
+fwq=open(FASTQ.Reader, leftRead)
+rvq=open(FASTQ.Reader, rightRead)
+
+#Prepare reads for graphing
+#Use {DNAAlphabet{4}} to handle N's
+ds=PairedReads{DNAAlphabet{4}}(fwq, rvq, "ecoli_test_paired", "ecoli_test", 75, 400, 0, FwRv)
+
+#Add reads
+add_paired_reads!(ws,ds)
+
+dbg!(ws, BigDNAMer{27}, 10, Symbol("ecoli_test"))
+
+#Clean up graph
+remove_tips!(ws, 200)
+
+#Generate de bruijn graph
+GenomeGraphs.Graphs.write_to_gfa1(ws.sdg, outputGraph)
--- practical.sh
+++ practical.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
-#$ -M <you>@nd.edu
+#$ -M ebrooks5@nd.edu
 #$ -m abe
 #$ -pe smp 8
 #$ -N BIOS60132_Practical_Three
 
+#Load modules
 module load bio/2.0
 module load julia
+
+#Set inputs
+leftRead="/tmp/ND_ICG_AUG_24/SRR2584863_1.trim.fastq"
+rightRead="/tmp/ND_ICG_AUG_24/SRR2584863_2.trim.fastq"
+
+#Set output
+outputGraph="ecoli_deBruijnGraph"
+
+#Run julia script for generating a
+# genome de bruijn graph
+julia ./generateDeBruijnGraph.jl $leftRead $rightRead $outputGraph
