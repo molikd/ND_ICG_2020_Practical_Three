3749910984ebcbb6022e7711d594639839fadcce
Nitin Vincent <nvincen2@crcfe01.crc.nd.edu>
2020-11-09 00:24:09 -0500

    done

--- juliascript.jl
+++ juliascript.jl
@@ -1,21 +1,43 @@
 #!/usr/bin/env julia         
 
+#$ -M nvincen2@nd.edu
+#$ -m abe
+#$ -N julia-debruijn
+
 using ArgParse
 using GenomeGraphs
 using FASTX
 using ReadDatastores
+using Graphs
+
+s = ArgParseSettings()
+@add_arg_table! s begin
+	"--left"
+	"--right"
+	"--pwork1"
+	"--pwork2"
+	"--out"
+end
+
+parsed_args = parse_args(ARGS, s)
+
+left_read = parsed_args["left"]
+right_read = parsed_args["right"]
+working1 = parsed_args["pwork1"]
+working2 = parsed_args["pwork2"]
+output = parsed_args["out"]
 
 ws=WorkSpace()
 
-fwq=open(FASTQ.Reader, leftRead)
-rvq=open(FASTQ.Reader, rightRead)
+fwq=open(FASTQ.Reader, left_read)
+rvq=open(FASTQ.Reader, right_read)
 
-ds=PairedReads{DNAAlphabet{4}}(fwq, rvq, "ecoli_test_paired", "ecoli_test", 75, 400, 0, FwRv)
+ds=PairedReads{DNAAlphabet{4}}(fwq, rvq, working1, working2, 75, 400, 0, FwRv)
 
 add_paired_reads!(ws,ds)
 
-dbg!(ws, BigDNAMer{27}, 10, Symbol("ecoli_test"))
+dbg!(ws, BigDNAMer{27}, 10, Symbol(working2))
 
 remove_tips!(ws, 200)
 
-GenomeGraphs.Graphs.write_to_gfa1(ws.sdg, parsed_args["out"])
+GenomeGraphs.Graphs.write_to_gfa1(ws.sdg, output)
--- practical.sh
+++ practical.sh
@@ -6,5 +6,6 @@
 
 module load bio/2.0
 module load julia
-julia ./juliascript.jl --left "SRR2584863_1.trim.fastq" --right "SRR2584863_2.trim.fastq" --out "deBruijnGraph"
+
+julia juliascript.jl --left "SRR2584863_1.trim.fastq" --right "SRR2584863_2.trim.fastq" --pwork1 "ecoli_test_paired1" --pwork2 "ecoli_test1" --out "deBruijnGraph1"
 
